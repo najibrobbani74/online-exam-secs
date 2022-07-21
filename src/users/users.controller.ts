8ad2ed6eb574fa021c5e388c530516ca5118cc1c
Pangeran Jonathan <pangeranjonathan1@gmail.com>
2022-07-21 21:28:19 +0700

    modified

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get,Post,Body } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Get()
+  getUserData() {
+    return this.userService.getUserData();
+  }
+
+  @Post('/register')
+  generateUser(
+    @Body('role') role: string,
+    @Body('name') name: string,
+    @Body('nim') nim: any,
+    @Body('password') password: any,
+    @Body('confPassword') confPassword: any,
+  ) {
+    return this.userService.generateUser(
+      role,
+      name,
+      nim,
+      password,
+      confPassword,
+    );
+  }
+
+  @Post('/login')
+  loginUser(@Body('nim') nim: string, @Body('password') password: string) {
+    return this.userService.loginUser(nim, password);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,NotFoundException } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private user: any[] = [];
+
+  getUserData(): any[] {
+    return this.user;
+  }
+
+  generateUser(
+    role: string,
+    name: string,
+    nim: string,
+    password: string,
+    confPassword: string,
+  ) {
+    if (password !== confPassword) {
+      throw new NotFoundException(
+        `password and confirm password does not match`,
+      );
+    }
+    this.user.push({
+      role,
+      name,
+      nim,
+      password,
+      confPassword,
+    });
+  }
+
+  loginUser(nim: string, password: string) {
+    const findUserByNim = this.user.findIndex((data) => data.nim === nim);
+    const findUserByPassword = this.user.findIndex(
+      (data) => data.password === password,
+    );
+    if (findUserByNim === -1 || findUserByPassword === -1) {
+      throw new NotFoundException(`nim and password is not found`);
+    }
+  }
+}
