174a3f81abcf65b2230e49b465b5ca7b54e11eaf
Pangeran Jonathan <pangeranjonathan1@gmail.com>
2022-07-23 15:00:51 +0700

    add dotenv

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './entity/users.entity';
-import { UserRepository } from './repository/users.repository';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
