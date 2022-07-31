9f6c0707b6b36fb59a3f9997339d50a778f189d3
najibrobbani74 <najibrobbani74@gmail.com>
2022-07-31 07:30:28 +0700

    kosongan

--- src/config/connectiondb.config.ts
+++ src/config/connectiondb.config.ts
@@ -4,12 +4,13 @@ import { Forms } from "src/forms/entity/forms.entity"
 import { Post } from "@/post/entity/post.entity"
 const dataSource = new DataSource(
     {
+        url: "postgres://lrcerexfbelyle:f556de33f50a98ce95001ed61fc563cc018521d80498fdbe311bece026d550a0@ec2-54-161-255-125.compute-1.amazonaws.com:5432/dd96t00bn0rsmm",
         type: "postgres",
-        host: "ec2-54-161-255-125.compute-1.amazonaws.com",
-        port: 5432,
-        username: "lrcerexfbelyle",
-        password: "f556de33f50a98ce95001ed61fc563cc018521d80498fdbe311bece026d550a0",
-        database: "dd96t00bn0rsmm",
+        // host: "ec2-54-161-255-125.compute-1.amazonaws.com",
+        // port: 5432,
+        // username: "lrcerexfbelyle",
+        // password: "f556de33f50a98ce95001ed61fc563cc018521d80498fdbe311bece026d550a0",
+        // database: "dd96t00bn0rsmm",
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
         // type: "postgres",
